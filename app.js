b1e30998576e9752e42ea091112699f578501f16
midhunx01 <midhunxda.123@gmail.com>
2022-05-25 17:10:03 +0530

    added Mongoose DataBase To Login And Signup

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require('ejs')
+const mongoose = require('mongoose');
+const req = require("express/lib/request");
+
 
 const app = express();
 
@@ -10,7 +13,7 @@ app.set("view engine","ejs")
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
-
+mongoose.connect("mongodb://localhost:27017/userData",{useNewUrlParser:true});
 
 
 app.get("/", function(req,res){
@@ -20,6 +23,57 @@ app.get("/", function(req,res){
 app.get("/signup", function(req,res){
   res.render("signup")
 });
+
+
+app.get("/login", function(req,res){
+  res.render("login")
+});
+
+app.post("/signup",function(req,res){
+  const user = new User ({
+    firstName : req.body.firstname,
+    lastName : req.body.lastname,
+    email : req.body.email,
+    password : req.body.password
+});
+
+user.save(function(err){
+  if (err){
+    console.log(err);
+  }else{
+    res.render("success")
+  }
+});
+});
+
+app.post("/login",function(req,res){
+   const username = req.body.email;
+   const password = req.body.password;
+
+   User.findOne({email : username},function(err,foundUser){
+     if (err){
+       console.log(err);
+     }else {
+       if (foundUser){
+         if (foundUser.password === password){
+           res.render("homepage")
+         }
+       }
+     }
+   })
+})
+
+const userSchema = new mongoose.Schema({
+     firstName : String,
+     lastName : String,
+     email : String,
+     password : String
+});
+
+const User = mongoose.model("user",userSchema);
+
+
+
 
 
 app.listen(3000,function(){
